88559783e1dc35d4b8bcc1f4db3ac6bda20ad8ef
Polina Fedorova <ps.fedorova@yandex.ru>
2020-04-13 01:48:26 +0500

    refactor: поправить объявление переменных

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // 1 Открыть попап
 
-let popup = document.querySelector('.popup'); // объявили попап
-let profileButtonEdit = document.querySelector('.profile__button-edit'); // объявили кнопку "Редактировать профиль"
+const popup = document.querySelector('.popup'); // объявили попап
+const profileButtonEdit = document.querySelector('.profile__button-edit'); // объявили кнопку "Редактировать профиль"
 
 function openPopup() { //открыть
-  popup.classList.add('popup_opened'); // добавили модификатор
-  popup.classList.remove('popup_closed'); // удалили модификатор
+    popup.classList.add('popup_opened'); // добавили модификатор
+    popup.classList.remove('popup_closed'); // удалили модификатор
 }
 profileButtonEdit.addEventListener('click', openPopup); // Метод addEventListener() присоединяет обработчик события к определенному элементу
 
@@ -13,38 +13,38 @@ profileButtonEdit.addEventListener('click', openPopup); // Метод addEventLi
 // 2 Редактировать поля ввода
 
 // 2.1 Находим поля формы в DOM (попап)
-let nameInput = document.querySelector('.popup__input_user-name'); 
-let jobInput = document.querySelector('.popup__input_user-about'); 
+let nameInput = document.querySelector('.popup__input_user-name');
+let jobInput = document.querySelector('.popup__input_user-about');
 // 2.2 Выбераем элементы, куда должны быть вставлены значения полей
 let name = document.querySelector('.profile__user-info-name');
 let job = document.querySelector('.profile__user-info-about');
 // 2.3 Форме ввода из попапа задаем значения "по умолчанию" со страницы
 function reset() {
-  nameInput.value = name.textContent;
-  jobInput.value = job.textContent;
-  }
-  reset();
+    nameInput.value = name.textContent;
+    jobInput.value = job.textContent;
+}
+reset();
 
 
 // 3 Закрыть попап
 
-let popupClose = document.querySelector('.popup__close'); // объявили крестик
+const popupClose = document.querySelector('.popup__close'); // объявили крестик
 
 function closePopup() { // закрыть крестиком
-  popup.classList.add('popup_closed');
-  popup.classList.remove('popup_opened');
-  reset();
+    popup.classList.add('popup_closed');
+    popup.classList.remove('popup_opened');
+    reset();
 }
 popupClose.addEventListener('click', closePopup);
 
 //закрыть кликом по фону - разобраться как это сделать
 
 function closeEscPopup(evt) { // закрыть кнопкой Esc
-  if (evt.keyCode == 27) {
-      popup.classList.add('popup_closed');
-      popup.classList.remove('popup_opened');
-      reset();
-  }
+    if (evt.keyCode == 27) {
+        popup.classList.add('popup_closed');
+        popup.classList.remove('popup_opened');
+        reset();
+    }
 }
 document.addEventListener('keydown', closeEscPopup);
 
@@ -52,19 +52,19 @@ document.addEventListener('keydown', closeEscPopup);
 // 4 Сохраненить изменения
 
 function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
 
-  name.textContent = nameInput.value
-  job.textContent = jobInput.value
-  closePopup();
-  closeEscPopup(evt);
+    name.textContent = nameInput.value
+    job.textContent = jobInput.value
+    closePopup();
+    closeEscPopup(evt);
 }
 
 
 // Строчку ниже написала только чтобы в консоли не было ошибок.
-let formElement = document.querySelector('.popup__form'); 
+let formElement = document.querySelector('.popup__form');
 // 5  Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
