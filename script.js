4d0272bd02200c547ab2644c9c7079077c9d952b
Polina Fedorova <ps.fedorova@yandex.ru>
2020-04-18 02:08:51 +0500

    feat: закрыть попап кликом вне области

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const popup = document.querySelector('.popup'); // объявили попап
 const profileButtonEdit = document.querySelector('.profile__button-edit'); // объявили кнопку "Редактировать профиль"
 
 function openPopup() { //открыть
-  popup.classList.toggle('popup_opened'); // добавили модификатор
-  popup.classList.toggle('popup_closed'); // удалили модификатор
+    popup.classList.toggle('popup_opened'); // добавили модификатор
+    popup.classList.toggle('popup_closed'); // удалили модификатор
 }
 profileButtonEdit.addEventListener('click', openPopup); // Метод addEventListener() присоединяет обработчик события к определенному элементу
 
@@ -20,8 +20,8 @@ let name = document.querySelector('.profile__user-info-name');
 let job = document.querySelector('.profile__user-info-about');
 // 2.3 Форме ввода из попапа задаем значения "по умолчанию" со страницы
 function reset() {
-  nameInput.value = name.textContent;
-  jobInput.value = job.textContent;
+    nameInput.value = name.textContent;
+    jobInput.value = job.textContent;
 }
 reset();
 
@@ -31,20 +31,31 @@ reset();
 const popupClose = document.querySelector('.popup__close'); // объявили крестик
 
 function closePopup() { // закрыть крестиком
-  popup.classList.toggle('popup_closed');
-  popup.classList.toggle('popup_opened');
-  reset();
-}
-popupClose.addEventListener('click', closePopup);
-
-//закрыть кликом по фону - разобраться как это сделать
-
-function closeEscPopup(evt) { // закрыть кнопкой Esc
-  if (evt.keyCode === 27) {
     popup.classList.toggle('popup_closed');
     popup.classList.toggle('popup_opened');
     reset();
-  }
+}
+popupClose.addEventListener('click', closePopup);
+
+
+////////////////////////////////////////
+function closeSpacePopup(evt) { //закрыть кликом по фону
+    if (evt.target === popup) {
+        popup.classList.toggle('popup_closed');
+        popup.classList.toggle('popup_opened');
+        reset();
+    }
+}
+document.addEventListener('click', closeSpacePopup);
+
+
+////////////////////////////////////////
+function closeEscPopup(evt) { // закрыть кнопкой Esc
+    if (evt.keyCode === 27) {
+        popup.classList.toggle('popup_closed');
+        popup.classList.toggle('popup_opened');
+        reset();
+    }
 }
 document.addEventListener('keydown', closeEscPopup);
 
@@ -52,13 +63,13 @@ document.addEventListener('keydown', closeEscPopup);
 // 4 Сохраненить изменения
 
 function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
 
-  name.textContent = nameInput.value;
-  job.textContent = jobInput.value;
-  closePopup();
+    name.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+    closePopup();
 }
 
 
