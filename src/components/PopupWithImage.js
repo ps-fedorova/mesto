8f2041704d2947f16dff7f93f42b4a747ac90d6c
Polina Fedorova <ps.fedorova@yandex.ru>
2020-06-19 18:42:39 +0500

    refactor: убрать импорт в класс констант из внешних модулей

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,18 +1,16 @@
 import Popup from './Popup.js';
 
-const popupZoomCard = document.querySelector('.popup__zoom-card'); // попап "Картинка-зум"
-const popupImage = popupZoomCard.querySelector('.popup__image'); // картинка
-const popupCardName = popupZoomCard.querySelector('.popup__card-name'); // подпись к картинке
-
 export default class PopupWithImage extends Popup {
-  constructor(selectorPopup) {
-    super(selectorPopup);
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._popupImage = this._popupSelector.querySelector('.popup__image'); // картинка
+    this._popupCardName = this._popupSelector.querySelector('.popup__card-name'); // подпись к картинке
   }
 
   openPopupImage(evt) {
-    popupCardName.textContent = evt.target.alt;
-    popupImage.src = evt.target.src;
-    popupImage.alt = evt.target.alt;
+    this._popupCardName.textContent = evt.target.alt;
+    this._popupImage.src = evt.target.src;
+    this._popupImage.alt = evt.target.alt;
     super.open();
   }
 }
